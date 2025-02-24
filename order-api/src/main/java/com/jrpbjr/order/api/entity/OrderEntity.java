96a8ce008dc59263649fd88f2f44506fc353dd77
jrpbjr <jrpbjr@gmail.com>
2025-02-24 19:38:53 -0300

    Entidade para pedidos

---
+++ order-api/src/main/java/com/jrpbjr/order/api/entity/OrderEntity.java
@@ -0,0 +1,49 @@
+package com.jrpbjr.order.api.entity;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+
+public class OrderEntity {
+
+    private Long orderId;
+    private Long customerId;
+    private BigDecimal total;
+    private List<OrderItem> items;
+
+    public OrderEntity() {
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    public List<OrderItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<OrderItem> items) {
+        this.items = items;
+    }
+}
+
