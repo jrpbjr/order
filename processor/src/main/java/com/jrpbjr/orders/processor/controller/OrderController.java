cc2fa6f18c2d67b2e27a70d542676f8c2c7e748e
jrpbjr <jrpbjr@gmail.com>
2025-02-25 14:37:54 -0300

    configurado para documentacao

--- processor/src/main/java/com/jrpbjr/orders/processor/controller/OrderController.java
+++ processor/src/main/java/com/jrpbjr/orders/processor/controller/OrderController.java
@@ -4,6 +4,13 @@ import com.jrpbjr.orders.processor.controller.dto.ApiResponse;
 import com.jrpbjr.orders.processor.controller.dto.OrderResponse;
 import com.jrpbjr.orders.processor.controller.dto.PaginationResponse;
 import com.jrpbjr.orders.processor.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,11 +29,14 @@ public class OrderController {
         this.orderService = orderService;
     }
 
+    @Operation(
+            summary = "Listar pedidos de um cliente",
+            description = "Retorna uma lista paginada de pedidos de um cliente específico com o resumo do valor total dos pedidos."
+    )
     @GetMapping("/customers/{customerId}/orders")
     public ResponseEntity<ApiResponse<OrderResponse>> listOrders(@PathVariable("customerId") Long customerId,
-                                                                 @RequestParam(name = "page", defaultValue = "0") Integer page,
-                                                                 @RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize){
-
+              @RequestParam(name = "page", defaultValue = "0") Integer page,
+        @RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize){
 
         var pageResponse = orderService.findAllByCustomerId(customerId, PageRequest.of(page, pageSize));
         var totalOnOrders = orderService.findTotalOnOrdersByCustomerId(customerId);
